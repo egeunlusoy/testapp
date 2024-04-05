9b06c5dce7b06daf095ff43ddd555477179d4e71
EGE UNLUSOY s305007 <s305007@studenti.polito.it>
2024-04-05 14:57:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//import WebApp from '@twa-dev/sdk'
+
+let WebApp = window.Telegram.WebApp;
+WebApp.ready();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <button onClick={() => WebApp.showAlert("Hello World! ")}>
+            Show Alert
+        </button>
+              
       </header>
     </div>
   );
