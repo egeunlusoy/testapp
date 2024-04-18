b440889a3d280ce062c9a933beebd077035d6185
EGE UNLUSOY s305007 <s305007@studenti.polito.it>
2024-04-18 17:40:14 +0200

    added grammy and start command

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 //import WebApp from '@twa-dev/sdk'
+const { Bot } = require("grammy");
 
 let WebApp = window.Telegram.WebApp;
 WebApp.ready();
 
+const bot = new Bot("6948802463:AAGsGKxqyB_JECw7nciuEQHCgOBA_KE7ob4"); 
+
+// Reply to any message with "Hi there!".
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
+bot.command("start" , "Welcome to the Test MiniApp, a mini app to test features");
+
+bot.start();
 
 function App() {
-const button =  WebApp.InlineKeyboardButton.text("Inline button");
-  WebApp.enableClosingConfirmation();
-
+//const button =  WebApp.InlineKeyboardButton.text("Inline button");
+ // WebApp.enableClosingConfirmation();
+ // { button }
+  
   return (
     <div className="App">
       <header className="App-header">
-      { button }
+     
       <button onClick={() => WebApp.showAlert("Hello World! ")}>
             Show Alert
         </button>
